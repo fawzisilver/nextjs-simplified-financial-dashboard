1f18e19a113945545b55f757051e55bf8b93a723
fawzisilver <90504301+fawzisilver@users.noreply.github.com>
2024-10-21 22:25:57 -0600

    layout and pages

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
 import styles from '@/app/ui/home.module.css'
 import { lusitana } from '@/app/ui/fonts';
+import Image from 'next/image';
 
 export default function Page() {
   return (
@@ -12,6 +13,7 @@ export default function Page() {
       </div>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
         <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+        
         <div 
           className={styles.shape}
         />
@@ -31,6 +33,20 @@ export default function Page() {
         </div>
         <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
           {/* Add Hero Images Here */}
+          <Image 
+            src="/hero-desktop.png"
+            width={1000}
+            height={760}
+            className='hidden md:block' //on medium width its shown, less than that its hidden (desktop)
+            alt="Screenshots of the dashboard project showing desktop version"
+          />
+          <Image 
+            src="/hero-mobile.png"
+            width={560}
+            height={620}
+            className='block md:hidden' //on medium width its hidden, less than its its shown (mobile)
+            alt='mobile version'
+          />
         </div>
       </div>
     </main>
